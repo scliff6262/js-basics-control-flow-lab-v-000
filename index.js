23cb45f14c4bfd9f249e10af7b3b30f8cae6e949
Sean Clifford <sclifford6262@gmail.com>
2018-05-02 15:53:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 // Write your code in this file!
- function scuberGreetingForFeet(){
-   
+ function scuberGreetingForFeet(num){
+   if (num <= 400){
+     "This one is on me!"
+   } else if (num > 2000){
+     "I will gladly take your thirty bucks."
+     else if (num > 2500) {
+      "No can do."
+     }
+   }
  }
