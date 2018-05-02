1e8f8576549b6e2d79d50f615c31c81b8448f02e
Sean Clifford <sclifford6262@gmail.com>
2018-05-02 16:04:30 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Write your code in this file!
+ function scuberGreetingForFeet(num){
+   let answer;
+   if (num <= 400){
+     answer = "This one is on me!"
+   } else if (num > 2000 && num < 2500){
+     answer = "I will gladly take your thirty bucks."
+   } else if (num > 2500) {
+      answer = "No can do."
+   }
+   return answer
+ }
+
+ function ternaryCheckCity(city){
+   let answer = city === "NYC" ? "Ok, sounds good." : "No go.";
+   return answer
+ }
+
+ function switchOnCharmFromTip(tip){
+   let answer;
+   switch(tip){
+     case "generous":
+        answer = "Thank you so much."
+        break;
+     case "not as generous":
+        answer = "Thank you."
+        break;
+     default:
+        answer = "Bye."
+   }
+   return answer
+ }
